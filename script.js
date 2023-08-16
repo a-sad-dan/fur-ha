27051e5e9e525b36d19dd509eeee3c82d15ad9f0
Dan <danishasad91@gmail.com>
2023-08-16 21:46:04 +0530

    fixed writeToModal

--- script.js
+++ script.js
@@ -25,22 +25,35 @@ function showMessage(event) {
 }
 
 //Logic to mew the cat
-
-const catMessages = ['meoww meoww', '*purrs*','*claws at you*','we think it will last','thank you farha','that felt nice','i loved that','pls do some more','owie','*sleeps*','scratch my ears pls','myau','mewww','*stretches*']
-
+const catMessages = ['Meoww Meoww', '*Purrs*', '*Claws at you*', 'We think it will last', 'Thank you Farha', 'That felt nice', 'I loved that', 'Pls do some more', 'Owie', '*sleeps*', 'Scratch my ears pls', 'Myau', 'Mewww', '*stretches*','miyauuu']
 const cats = document.querySelectorAll('.cat');
 cats.forEach((cat, index) => cat.addEventListener('click', () => catSays()));
 
 function catSays() {
-    const message = catMessages[Math.floor(Math.random()*catMessages.length)]
-    const modal = document.querySelector('.cat.modal');
+    const message = catMessages[Math.floor(Math.random() * catMessages.length)]
+    writeToModal(message,900);
+}
+
+function writeToModal(message,time) {
+    const modal = document.querySelector('.modal');
     modal.textContent = message;
     modal.classList.remove('hidden');
 
     setTimeout(() => {
         modal.classList.add('hidden');
-    }, 1000);
+    }, time);
 }
+
+
+//Logic for the metro
+document.querySelector('.metro').addEventListener('click',metroSays);
+const metroMessages = ["you look cute together","Next station is Panchsheel park, get off","Bachon ka hath pakad ke rakhe (hold his hand)","mind the gap(never let him go)","transistor or a bag could be a bomb(u = bomb)","Do not sit on the floor of the train(dan = floor)","D<3F Fr Fr"]
+function metroSays() {
+    const message = metroMessages[Math.floor(Math.random() * metroMessages.length)];
+    writeToModal(message,1500);
+}
+
+
 
 
 
